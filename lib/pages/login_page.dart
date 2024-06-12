587327f3166a39f94f2413c64a39803bf49abc18
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-06-13 00:40:46 +0300

    a

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,30 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.lock_open_rounded,
-            size: 100,
-            color: Theme.of(context).colorScheme.inversePrimary,
-          ),
-          const SizedBox(height: 25),
-          Text(
-            "Food Delivery App",
-            style: TextStyle(
-              fontSize: 16,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
               color: Theme.of(context).colorScheme.inversePrimary,
             ),
-          ),
-          const SizedBox(height: 25),
-        ],
+            const SizedBox(height: 25),
+            Text(
+              "Food Delivery App",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            const SizedBox(height: 25),
+            MyTextField(
+                controller: emailController,
+                hintText: "Email",
+                obscureText: false),
+          ],
+        ),
       ),
     );
   }
