699cb14c80b6461f6271f48d24e43ee5eb634e30
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-06-12 23:51:04 +0300

    a

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.lock_open_rounded,
+            size: 100,
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          const SizedBox(height: 25),
+          Text(
+            "Food Delivery App",
+            style: TextStyle(
+              fontSize: 16,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+          const SizedBox(height: 25),
+        ],
+      ),
+    );
+  }
+}
