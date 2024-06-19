3d2101a16301dac813b856db91989858fcd54ce7
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-06-19 17:42:30 +0300

    a

---
+++ lib/components/my_cart_tile.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_quantity_selector.dart';
+import 'package:food_delivery_app/models/cart_item.dart';
+import 'package:provider/provider.dart';
+
+class MyCartTile extends StatelessWidget {
+  final CartItem cartItem;
+
+  const MyCartTile({
+    super.key,
+    required this.cartItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, restaurant, child) => Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    cartItem.food.imagePath,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(cartItem.food.name),
+                    Text("\$" + cartItem.food.price.toString()),
+                  ],
+                ),
+                MyQuantitySelector(
+                  quantity: quantity,
+                  food: food,
+                  onIncrement: onIncrement,
+                  onDecrement: onDecrement,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_cart_tile.dart';
 import 'package:food_delivery_app/models/restaurant.dart';
 import 'package:provider/provider.dart';
 
@@ -13,7 +14,24 @@ class CartPage extends StatelessWidget {
 
         return Scaffold(
           appBar: AppBar(
+            centerTitle: true,
             title: Text("Cart"),
+            backgroundColor: Colors.transparent,
+            foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: userCart.length,
+                  itemBuilder: (context, index) {
+                    final cartItem = userCart[index];
+
+                    return MyCartTile(cartItem: cartItem);
+                  },
+                ),
+              )
+            ],
           ),
         );
       },
