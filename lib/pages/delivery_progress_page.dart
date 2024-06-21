303b4e733cf62e25e5dad2907abe3dcec1af6fee
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-06-21 20:14:48 +0300

    a

--- lib/pages/delivery_progress_page.dart
+++ lib/pages/delivery_progress_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_receipt.dart';
 
 class DeliveryProgressPage extends StatelessWidget {
   const DeliveryProgressPage({super.key});
@@ -6,9 +7,14 @@ class DeliveryProgressPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
+        centerTitle: true,
         title: Text("Delivery in Progress.."),
         backgroundColor: Colors.transparent,
+      ),
+      body: Column(
+        children: [MyReceipt()],
       ),
     );
   }
