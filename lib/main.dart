42cba56c7ba868f05f5b98df9821ce16520419b4
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-06-25 12:29:31 +0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:food_delivery_app/auth/login_or_register_page.dart';
+import 'package:food_delivery_app/services/auth/login_or_register_page.dart';
 import 'package:food_delivery_app/firebase_options.dart';
 import 'package:food_delivery_app/models/restaurant.dart';
 import 'package:provider/provider.dart';
