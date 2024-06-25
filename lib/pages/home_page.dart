0db1fb47c446983725eab619eb4632e9740b4c7a
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-06-25 14:23:33 +0300

    a

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -80,7 +80,7 @@ class _HomePageState extends State<HomePage>
                   endIndent: 25,
                   color: Theme.of(context).colorScheme.secondary,
                 ),
-                const MyCurrentLocation(),
+                MyCurrentLocation(),
                 const MyDescriptionBox(),
               ],
             ),
