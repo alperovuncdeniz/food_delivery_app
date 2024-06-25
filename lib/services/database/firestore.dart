9929542da982eee13a5b248d61f1710743238b57
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-06-25 13:32:50 +0300

    a

--- lib/services/database/firestore.dart
+++ lib/services/database/firestore.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService {
   final CollectionReference orders =
-      FirebaseFirestore.instance.collection("orders");
+      FirebaseFirestore.instance.collection('orders');
 
   Future<void> saveOrderToDatabase(String receipt) async {
     await orders.add({
